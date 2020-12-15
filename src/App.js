71ea3a89ce0ef199ecfebd875c61adcab6428496
peacefrog <gregroyclark@gmail.com>
2020-12-14 19:56:09 -0600

    adds ChatRoom component

--- src/App.js
+++ src/App.js
@@ -56,4 +56,11 @@ function SignOut() {
   )
 }
 
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+}
+
 export default App;
