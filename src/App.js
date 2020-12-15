9ae6c1522ac16931566ac5386e2bc5ba7128932a
peacefrog <gregroyclark@gmail.com>
2020-12-14 19:22:17 -0600

    adds firebase connection values to app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-
+  apiKey: "AIzaSyC6Huxd6r27tWbeu9ah7j94FyvRCyPHUSo",
+  authDomain: "chatter-ae437.firebaseapp.com",
+  projectId: "chatter-ae437",
+  storageBucket: "chatter-ae437.appspot.com",
+  messagingSenderId: "912557874646",
+  appId: "1:912557874646:web:8b4470e15a49a463151877",
+  measurementId: "G-331QFBZE2R"
 });
 
 const auth = firebase.auth();
